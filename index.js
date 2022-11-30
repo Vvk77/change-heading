d3b25b54f22a0e4dc3d7ee85c997c754cf09e8eb
Vvk77 <vvk.yadav12@gmail.com>
2022-11-30 21:52:06 +0530

    update:done

---
+++ index.js
@@ -0,0 +1,13 @@
+const inputEl = document.querySelector("#input");
+const outputEl = document.querySelector("#output");
+const h1 = document.querySelector("#h1");
+const h2 = document.querySelector("#h2");
+const h3 = document.querySelector("#h3");
+
+const change = (a) => {
+  outputEl.innerHTML = `<${a}>` + inputEl.value + `<${a}/>`;
+};
+
+h1.addEventListener("click", () => change("h1") );
+h2.addEventListener("click", () => change("h2") );
+h3.addEventListener("click", () => change("h3") );
