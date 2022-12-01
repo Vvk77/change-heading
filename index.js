5c9b6af249053f65088d13c64feb220dcd248f14
Vvk77 <vvk.yadav12@gmail.com>
2022-12-01 16:45:41 +0530

    update:done

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-const inputEl = document.querySelector("#input");
-const outputEl = document.querySelector("#output");
-const h1 = document.querySelector("#h1");
-const h2 = document.querySelector("#h2");
-const h3 = document.querySelector("#h3");
+const numOne = document.querySelector("#num1");
+const numTwo = document.querySelector("#num2");
+const add = document.querySelector("#add");
+const sub = document.querySelector("#sub");
+const mul = document.querySelector("#mul");
+const div = document.querySelector("#div");
+const ans = document.querySelector("#ans");
 
-const change = (a) => {
-  outputEl.innerHTML = `<${a}>` + inputEl.value + `<${a}/>`;
-};
 
-h1.addEventListener("click", () => change("h1") );
-h2.addEventListener("click", () => change("h2") );
-h3.addEventListener("click", () => change("h3") );
+function addition(){
+  var number1 = Number(num1.value);
+  var number2 = Number(num2.value);
+  var result = 0;
+  result = number1 + number2;
+  ans.innerHTML = result;
+
+}
+function subtraction(){
+  var number1 = Number(num1.value);
+  var number2 = Number(num2.value);
+  var result = 0;
+  result = number1 - number2;
+  ans.innerHTML = result;
+
+}
+function multiply(){
+  var number1 = Number(num1.value);
+  var number2 = Number(num2.value);
+  var result = 0;
+  result = number1 * number2;
+  ans.innerHTML = result;
+
+}
+function divide (){
+  var number1 = Number(num1.value);
+  var number2 = Number(num2.value);
+  var result = 0;
+  result = number1 / number2;
+  ans.innerHTML = result;
+
+}
+
+
+add.addEventListener("click", addition);
+sub.addEventListener("click", subtraction);
+mul.addEventListener("click", multiply);
+div.addEventListener("click", divide );
